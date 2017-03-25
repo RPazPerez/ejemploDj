4d90ca21704be5cde5e3854b9dd0abdc920bf185
Jesus Gonzalez <fj-gonzalez-23@hotmail.com>
2017-03-25 03:15:42 -0600

    Se termino todo el registro y el login

--- app/hackaton/views.py
+++ app/hackaton/views.py
@@ -28,5 +28,38 @@ def login_aut(request):
     else:
         return redirect('/')
 
+def registro_registrar(request):
+    if request.method == "POST":
+        array_errores = []
+        diccionario = {}
+        nombre = request.POST.get('name')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        banera = request.POST.get('banera')
+        regadera = request.POST.get('regadera')
+        ahorrador = request.POST.get('ahorrador')
+        cont = 0
+        usuario = User.objects.filter(username=nombre)
+        for datos in usuario:
+            if datos.email == email:
+                array_errores.append("Email ya registrado")
+            cont = cont + 1
+        if cont == 0:
+            usuario = User.objects.create_user(username=nombre, password=password, email=email)
+            diccionario["mensaje"] = "Acceso"
+            return JsonResponse(diccionario)
+        else:
+            array_errores.append("Usuario ya registrado")
+            diccionario["errores"] = array_errores
+        return JsonResponse(diccionario)
+    else:
+        return redirect('/')
+
 def registro_view(request):
-    return HttpResponse("Hola")
+    return render(request, "registro.html")
+
+def profile_view(request):
+    return render(request, "profile.html")
+
+def profile_view_modificar(request):
+    return render(request, "editar.html")
